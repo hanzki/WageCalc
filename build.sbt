3d3abb7632696d54803269de97ef50ac111e1c30
Hannu Huhtanen <hannu.huhtanen@gmail.com>
2014-12-14 00:40:16 +0200

    Added unit tests and fixed bug in WorkShift for early morning hours

--- build.sbt
+++ build.sbt
@@ -5,3 +5,5 @@ version := "1.0"
 scalaVersion := "2.11.4"
 
 libraryDependencies += "com.github.nscala-time" %% "nscala-time" % "1.6.0"
+
+libraryDependencies += "org.scalatest" % "scalatest_2.11" % "2.2.1" % "test"
--- src/main/scala/WorkShift.scala
+++ src/main/scala/WorkShift.scala
@@ -9,13 +9,21 @@ class WorkShift(val interval: Interval) {
     throw new IllegalArgumentException("Workshifts can't be over 24 hours long")
 
   private val night = new Interval(interval.getStart.withTime(18,0,0,0), Duration.standardHours(12))
+  private val earlyMorning = new Interval(interval.getStart.withTimeAtStartOfDay(), interval.getStart.withTime(6,0,0,0))
 
   def hours = interval.toDuration.getStandardMinutes / 60.0d
 
-  def eveningHours =
-    Option(night.overlap(interval)) match {
-    case Some(overlap) => overlap.toDuration.getStandardMinutes / 60.0d
-    case None => 0d
+  def eveningHours = {
+    val atNightMinutes = Option(night.overlap(interval)) match {
+      case Some(ol) => ol.toDuration.getStandardMinutes
+      case None => 0d
+    }
+    val inTheMorningMinutes = Option(earlyMorning.overlap(interval)) match {
+      case Some(ol) => ol.toDuration.getStandardMinutes
+      case None => 0d
+    }
+
+    (atNightMinutes + inTheMorningMinutes) / 60.0d
   }
 
 }
